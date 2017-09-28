d4f6e12b2fe6092056755fd64e960fd7053fdb35
Caesar Ho <32213901+CaesarHo@users.noreply.github.com>
2017-09-28 17:56:07 +0800

    发布版本,修改首页

--- app/src/main/java/com/home/yoosee/MainActivity.java
+++ app/src/main/java/com/home/yoosee/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
