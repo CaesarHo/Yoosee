9f861f102a516493a691041de843ca8a3859da4e
Caesar Ho <332609358@qq.com>
2017-10-03 18:59:56 +0800

    发布版本20171003更新代码 1.配置butterknife 2.添加侧滑菜单

--- app/src/main/java/com/home/yoosee/MainActivity.java
+++ app/src/main/java/com/home/yoosee/MainActivity.java
@@ -3,21 +3,23 @@ package com.home.yoosee;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.home.yoosee.widget.CoordinatorMenu;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.ibtn_left)
+    ImageButton ibtnLeft;
+    private CoordinatorMenu mCoordinatorMenu;
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -45,16 +47,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        ButterKnife.bind(this);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //侧滑代码
+        mCoordinatorMenu = (CoordinatorMenu) findViewById(R.id.menu);
     }
 
+    @OnClick({R.id.ibtn_left})
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ibtn_left:
+                if (mCoordinatorMenu.isOpened()) {
+                    mCoordinatorMenu.closeMenu();
+                } else {
+                    mCoordinatorMenu.openMenu();
+                }
+                break;
+        }
+    }
+
+
+    //退出时间
+    private long  currentBackPressedTime= 0;
+    //退出间隔
+    private static final int BACK_PRESSED_INTERVAL= 2000;
     @Override
     public void onBackPressed() {
+        if (mCoordinatorMenu.isOpened()) {
+            mCoordinatorMenu.closeMenu();
+        } else {
+            //判断时间间隔
+            if(System.currentTimeMillis()-currentBackPressedTime>BACK_PRESSED_INTERVAL) {
+                currentBackPressedTime= System.currentTimeMillis();
+                Toast.makeText(this,"再按一次返回键退出程序", Toast.LENGTH_SHORT).show();
+            }else{
+                //退出
+                finish();
+            }
+            super.onBackPressed();
+        }
+
 //        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 //        if (drawer.isDrawerOpen(GravityCompat.START)) {
 //            drawer.closeDrawer(GravityCompat.START);
